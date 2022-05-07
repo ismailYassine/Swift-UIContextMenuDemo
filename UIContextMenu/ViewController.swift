d6b7c1f3a5056387c9d8080ab588ca7f5d94d1b4
Yassine Ismail <yassine.iguire@gmail.com>
2022-05-07 11:57:36 -0400

    ContextMenu implemented

--- UIContextMenu/ViewController.swift
+++ UIContextMenu/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var didTapContextMenuButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        contextMenu()
     }
 
+    func contextMenu() {
+        let actionOne = UIAction(title: "Action One", image:nil) { (action) in
+           // Code to execute after click
+                  }
+        
+        let actionTwo = UIAction(title: "Action Two", image:nil) { (action) in
+            // Code to execute after click
+        }
+                
+        let menu =  UIMenu(title: "Title", options: .displayInline, children: [actionOne, actionTwo])
+
+        didTapContextMenuButton.menu = menu
+        didTapContextMenuButton.showsMenuAsPrimaryAction = true
+        
+    }
 
 }
 
